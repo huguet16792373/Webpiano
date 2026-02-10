85c2fd083d4d9809682fef4c01080b07a1d40905
uge <251zs04231@student.zen.ac.jp>
2026-02-10 21:57:45 +0900

    firstcommit

---
+++ piano.js
@@ -0,0 +1,227 @@
+const canvas = document.getElementById("canvasArea");
+let ctx=null;
+
+const AudioCtx=window.AudioContext||window.webkitAudioContext;
+let audioCtx=null;
+function getAudioCtx(){
+  if(!audioCtx){
+    audioCtx = new (window.AudioContext||window.webkitAudioContext)();
+  }
+  return audioCtx;
+}
+
+
+const wkeys=[];
+const bkeys=[];
+const wkeyWidth=50;
+const wkeyHeight=100;
+const bkeyWidth=25;
+const bkeyHeight=60;
+
+const A4=440;
+
+class keyObject{
+  constructor(x,y,c,semitone){
+    this.x=x;
+    this.y=y;
+    this.c=c;
+    this.semitone=semitone;
+    this.freq=A4*Math.pow(2,semitone/12);
+    this.status="inactive";
+  }
+  get width(){return this.c===0?wkeyWidth:bkeyWidth};
+  get height(){return this.c===0?wkeyHeight:bkeyHeight};
+  draw(){
+    if(this.c===0){
+      switch(this.status){
+        case "inactive":
+          ctx.fillStyle="white";
+          break;
+        case "active":
+          ctx.fillStyle="red";
+          break;
+        case "hovered":
+          ctx.fillStyle="gray";
+          break;
+      }
+      ctx.fillRect(this.x,this.y,wkeyWidth,wkeyHeight);
+      ctx.strokeRect(this.x,this.y,wkeyWidth,wkeyHeight);
+      
+    }
+    else {
+      switch(this.status){
+        case "inactive":
+          ctx.fillStyle="black";
+          break;
+        case "active":
+          ctx.fillStyle="red";
+          break;
+        case "hovered":
+          ctx.fillStyle="gray";
+          break;
+      }
+      ctx.fillRect(this.x,this.y,bkeyWidth,bkeyHeight)
+    }
+  };
+    testHit(point){
+    return (
+      point.x >= this.x && point.x <= this.x + this.width
+      &&
+      point.y >= this.y && point.y <= this.y + this.height
+    );
+  }
+
+  startSound(){
+  const ctx = getAudioCtx();
+  if(ctx.state === "suspended"){
+    ctx.resume();
+  }
+  if(this.osc) return;
+
+  const osc = audioCtx.createOscillator();
+  const gain = audioCtx.createGain();
+
+  osc.type = "sine";
+  osc.frequency.value = this.freq;
+
+  osc.connect(gain);
+  gain.connect(audioCtx.destination);
+
+  gain.gain.setValueAtTime(0.001, audioCtx.currentTime);
+  gain.gain.exponentialRampToValueAtTime(1, audioCtx.currentTime + 0.02);
+
+  osc.start();
+  this.osc = osc;
+  this.gain = gain;
+}
+
+stopSound(){
+  if(!this.osc) return;
+
+  this.gain.gain.exponentialRampToValueAtTime(
+    0.001,
+    audioCtx.currentTime + 0.05
+  );
+  this.osc.stop(audioCtx.currentTime + 0.05);
+  this.osc = null;
+}
+
+}
+function playnote(freq,duration=0.5){
+  const osc=audioCtx.createOscillator();
+  const gain =audioCtx.createGain();
+
+  osc.type="sine";
+  osc.frequency.value=freq;
+
+  osc.connect(gain);
+  gain.connect(audioCtx.destination);
+
+  osc.start();
+  gain.gain.setValueAtTime(1,audioCtx.currentTIme);
+   gain.gain.exponentialRampToValueAtTime(
+    0.001,
+    audioCtx.currentTime + duration
+  );
+  osc.stop(audioCtx.currentTime + duration);
+}
+function init(){
+if (canvas.getContext){
+  canvas.width=800;
+    canvas.height=200;
+    ctx = canvas.getContext("2d");
+}
+getAudioCtx();
+const whiteSemitone=[-9,-7,-5,-4,-2,0,2];
+for(let i=0;i<13;i++){
+  const octave =Math.floor(i/7);
+  const s=whiteSemitone[i%7]+octave*12;
+  wkeys.push(new keyObject(50+50*i,50,0,s));
+}
+for(let i=0;i<2;i++){
+  const blackSemitone=[-8,-6,-3,-1,1];
+  bkeys.push(new keyObject(175/2+50*7*i,50,1,blackSemitone[0]+i*12));
+  bkeys.push(new keyObject(175/2+50+50*7*i,50,1,blackSemitone[1]+i*12));
+  bkeys.push(new keyObject(475/2+50*7*i,50,1,blackSemitone[2]+i*12));
+  bkeys.push(new keyObject(475/2+50+50*7*i,50,1,blackSemitone[3]+i*12));
+  bkeys.push(new keyObject(475/2+100+50*7*i,50,1,blackSemitone[4]+i*12));
+}
+for(let key of wkeys)key.draw();
+for(let key of bkeys)key.draw();
+}
+function redraw(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  for(let key of wkeys)key.draw();
+  for(let key of bkeys)key.draw();
+}
+init();
+canvas.addEventListener("mousedown",(e)=>{
+  const rect=canvas.getBoundingClientRect();
+
+  const point={
+    x:e.clientX-rect.left,
+    y:e.clientY-rect.top
+  };
+  [...wkeys, ...bkeys].forEach(k => k.status = "inactive");
+  for(let key of bkeys){
+    if(key.testHit(point)){
+      key.status="active";
+      console.log("黒鍵押下中");
+      key.startSound();
+      redraw();
+      return;
+    }
+  }
+  for(let key of wkeys){
+    if(key.testHit(point)){
+      key.status="active";
+      console.log("白鍵押下中");
+      key.startSound();
+      redraw();
+      return;
+    }
+  }
+
+})
+window.addEventListener("mouseup",(e)=>{
+  [...wkeys,...bkeys].forEach(k=>{
+    if(k.status==="active"){
+      k.status="inactive";
+      k.stopSound();}
+  })
+  redraw();
+})
+
+canvas.addEventListener("mousemove",(e)=>{
+  const rect=canvas.getBoundingClientRect();
+
+  const point={
+    x:e.clientX-rect.left,
+    y:e.clientY-rect.top
+  };
+   [...wkeys, ...bkeys].forEach(k => {
+    if(k.status === "hovered") k.status = "inactive";
+  });
+  for(let key of bkeys){
+    if(key.testHit(point)&&key.status!=="active"){
+      key.status="hovered";
+      redraw();
+      return;
+    }
+  }
+  for(let key of wkeys){
+    if(key.testHit(point)&&key.status!=="active"){
+      key.status="hovered";
+      redraw();
+      return;
+    }
+  }
+  
+})
+canvas.addEventListener("mouseleave", ()=>{
+  [...wkeys, ...bkeys].forEach(k => {
+    if(k.status === "hovered") k.status = "inactive";
+  });
+  redraw();
+});
+
